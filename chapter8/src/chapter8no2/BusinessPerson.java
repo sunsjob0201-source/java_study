39dc5ca75b5d9aee6bc8e453d92fae7ba8388a78
6D13 <6D13@PC-6D13>
2026-05-20 13:26:41 +0900

    upload

---
+++ chapter8/src/chapter8no2/BusinessPerson.java
@@ -0,0 +1,9 @@
+package chapter8no2;
+
+public class BusinessPerson extends Person {
+
+	public String work() {
+		return String.format("%d歳の%sは、働きます。", this.age, this.name);
+		
+	}
+}
---
+++ chapter8/src/chapter8no2/InheritBasic.java
@@ -0,0 +1,14 @@
+package chapter8no2;
+
+public class InheritBasic {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+    var bp = new BusinessPerson();
+    bp.name = "山田太郎";
+    bp.age = 30;
+    System.out.println(bp.show());
+    System.out.println(bp.work());
+	}
+
+}
